e403915bbae57a014e6b4d8695d44b9ae916bed8
sohyeon-VIRNECT <ash_h@virnect.com>
2024-07-29 11:16:33 +0900

    style: global 스타일 추가
    * reset styles 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { Global } from '@emotion/react'
+import * as globalStyles from './styles/global.ts'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
 )
