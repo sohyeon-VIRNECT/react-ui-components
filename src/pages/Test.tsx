9474fc0f1b0f0358197c9ba672ba04ec8d9e5a32
sohyeon-VIRNECT <ash_h@virnect.com>
2024-07-31 14:06:38 +0900

    refactor: 테스트용으로 표출한 컴포넌트를 /test 페이지로 이동

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,5 +1,112 @@
+import Text from '@/components/Text'
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+import TextField from '@/components/TextField'
+import Alert from '@/components/Alert'
+import { useAlertContext } from '@/contexts/AlertContext'
+// import FixedBottomButton from '@/components/FixedBottomButton'
+import Spacing from '@/components/Spacing'
+import Skeleton from '@/components/Skeleton'
+
 function Test() {
-  return <div>TestPage</div>
+  const { open } = useAlertContext()
+
+  return (
+    <>
+      <Text typography="t1" display="block" color="red">
+        t1
+      </Text>
+      <Text typography="t2" color="blue">
+        t2
+      </Text>
+      <Text typography="t3" textAlign="right" display="block">
+        t3
+      </Text>
+      <Text typography="t4" display="block" bold>
+        t4
+      </Text>
+      <Text>t5</Text>
+      <Text typography="t6" color="green">
+        t6
+      </Text>
+      <Text typography="t7" display="inline-block" color="grey">
+        t7
+      </Text>
+
+      <Spacing size={40} />
+
+      <Button>default</Button>
+      <Button color="success" full>
+        success full
+      </Button>
+      <Button color="error">error</Button>
+      <br />
+      <Button outline>outline</Button>
+      <Button color="success" outline>
+        success outline
+      </Button>
+      <Button color="error" outline full>
+        error outline full
+      </Button>
+      <Button disabled>disabled</Button>
+      <Button size="medium">medium</Button>
+      <Button size="large">large</Button>
+
+      <Spacing size={40} />
+
+      <Input placeholder="로그인" aria-invalid={false} />
+      <Input aria-invalid={true} />
+
+      <Spacing size={40} />
+
+      <TextField label="아이디" />
+      <TextField
+        label="비밀번호"
+        hasError={true}
+        helpMessage={'비밀번호는 8글자 이상 입력해주세요.'}
+        type="password"
+      />
+
+      <Spacing size={40} />
+
+      <Alert
+        title="알럿입니다"
+        description="설명입니다."
+        buttonLabel="good"
+        onButtonClick={() => {}}
+        open={false}
+      />
+
+      <Button
+        onClick={() => {
+          open({
+            title: '제목',
+            description: '내용입니다.',
+            onButtonClick: () => {
+              //
+            },
+          })
+        }}
+      >
+        Alert 열기
+      </Button>
+
+      <Spacing size={40} />
+
+      {/* <FixedBottomButton label="신청하기" onClick={() => {}} /> */}
+
+      <Spacing size={40} />
+
+      <Button.Group title="버튼 그룹">
+        <Button>하나</Button>
+        <Button>둘</Button>
+        <Button>셋</Button>
+      </Button.Group>
+
+      <Spacing size={40} />
+      <Skeleton width={200} height={120} />
+    </>
+  )
 }
 
 export default Test
